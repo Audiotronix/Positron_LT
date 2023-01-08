a8becfd7c796433e65e3881531c21031052d1b31
Fliens <35639879+Fliens@users.noreply.github.com>
2023-01-08 14:30:21 +0100

    removed blue diamond

--- .github/workflows/create_readme.py
+++ .github/workflows/create_readme.py
@@ -20,7 +20,7 @@ for row in csv_data:
     if entry['type'] == 'printed':
         printed_table += '| '+(str(entry['cad_name']) if entry['part_name'] == '---' else str(entry['part_name']) )+ ' | [STL](./Printed%20Parts/STL/'+str(entry['cad_name'])+'.stl) | [STEP](./Printed%20Parts/STEP/'+str(entry['cad_name'])+'.step) | '+str(entry['amount'])+' | '+str(entry['note'].split('[t:')[1].split(';w:')[0])+' | '+str(entry['note'].split('[t:')[1].split(';w:')[1].split(']')[0])+' |\n'
     elif entry['type'] == 'mechanical':
-        mechanical_table += '| ['+(str(entry['cad_name']) if entry['part_name'] == '---' else str(entry['part_name']) )+'](./Mechanical%20Parts/'+str(entry['cad_name'])+'.stl) | ['+(':large_blue_diamond:' if str(entry['link']) != '---' else ':small_red_triangle:')+']('+str(entry['link'])+') | ['+(':large_blue_diamond:' if str(entry['alt_link']) != '---' else ':small_red_triangle:')+']('+str(entry['alt_link'])+') | '+str(entry['amount'])+' | '+str(entry['price'])+' | '+str(entry['note'])+' |\n'
+        mechanical_table += '| ['+(str(entry['cad_name']) if entry['part_name'] == '---' else str(entry['part_name']) )+'](./Mechanical%20Parts/'+str(entry['cad_name'])+'.stl) | ['+('link' if str(entry['link']) != '---' else ':small_red_triangle:')+']('+str(entry['link'])+') | ['+('link' if str(entry['alt_link']) != '---' else ':small_red_triangle:')+']('+str(entry['alt_link'])+') | '+str(entry['amount'])+' | '+str(entry['price'])+' | '+str(entry['note'])+' |\n'
 
 
 #README update
