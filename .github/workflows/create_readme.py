67892e7794dd998dacb7285b5c8a76358e7b5a92
Simon <35639879+Fliens@users.noreply.github.com>
2023-01-13 12:55:03 +0100

    Fix

--- .github/workflows/create_readme.py
+++ .github/workflows/create_readme.py
@@ -24,7 +24,7 @@ def write_mechanical(part_data) -> str:
         note = note.replace(url, '[link]('+url+')')
 
     part_name = '['+str(part_data['cad_name'])+'](./Mechanical%20Parts/'+str(part_data['cad_name'])+'.stl)'
-    link = ('[link]('+str(part_data['link'])+')') if str(part_data['link']) != '---' else: ':small_red_triangle:'
+    link = (('[link]('+str(part_data['link'])+')') if str(part_data['link']) != '---' else: ':small_red_triangle:')
 
     #add alt link to link
     if part_data['alt_link'] != '---':
