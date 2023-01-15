a453d04fd3e04f54a8a7c92adcfe0866ec652496
Fliens <35639879+Fliens@users.noreply.github.com>
2023-01-15 15:37:25 +0100

    Test

--- .github/workflows/create_readme.py
+++ .github/workflows/create_readme.py
@@ -97,7 +97,7 @@ mechanical_table = ''
 
 #create table header strings
 printed_header = '|'+pad_column('Part Name', column_lengths['printed']['cad_name'], 2.4)+'| STL | STEP |'+pad_column('Amount', column_lengths['printed']['amount'])+'| Print Time | Weight (g)|\n| --- | --- | --- | --- | --- | --- |\n'
-mechanical_header = '|'+pad_column('Part Name', column_lengths['mechanical']['cad_name'], 2.4)+'|'+pad_column('CAD Amount', column_lengths['mechanical']['amount'])+'| '+pad_column('Link', column_lengths['mechanical']['link'])+'|'+pad_column('PCS', column_lengths['mechanical']['pcs'])+' |'+pad_column('Price', column_lengths['mechanical']['price'], 2)+'|'+pad_column('Note', column_lengths['mechanical']['note'])+'|\n| --- | --- | --- | --- | --- | --- |\n'
+mechanical_header = '|'+pad_column('Part Name', column_lengths['mechanical']['cad_name'], 2.6)+'|'+pad_column('CAD Amount', column_lengths['mechanical']['amount'])+'| '+pad_column('Link', column_lengths['mechanical']['link'])+'|'+pad_column('PCS', column_lengths['mechanical']['pcs'])+' |'+pad_column('Price', column_lengths['mechanical']['price'], 2)+'|'+pad_column('Note', column_lengths['mechanical']['note'])+'|\n| --- | --- | --- | --- | --- | --- |\n'
 
 #create category title + table header
 for category in categories:
