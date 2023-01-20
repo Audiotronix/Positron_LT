77e89919235bee453b8e369b66f492595cea0f13
Simon <35639879+Fliens@users.noreply.github.com>
2023-01-20 15:42:11 +0100

    Fix

--- .github/workflows/create_readme.py
+++ .github/workflows/create_readme.py
@@ -172,7 +172,7 @@ with open('./Parts/README.md', "w") as f:
     line = next(lines_iter)
 
     # Fing begining of printed table
-    while '##' not in line and 'Printed Parts' not in line:
+    while '## [Printed Parts]' not in line:
         f.write(line)
         line = next(lines_iter)
     f.write(line)
